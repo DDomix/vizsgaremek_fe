2c576286fe7827e82a164782f815251d3096a4fa
Fuzi18 <fuzesi.t2003@]mail.com>
2023-02-14 15:31:20 +0100

    asdsad

---
+++ src/Login.tsx
@@ -0,0 +1,29 @@
+import { Component, ReactNode } from "react";
+
+import './loginstyle.css'
+
+export default class Login extends Component {
+    render(): ReactNode {
+        return <div className="login-box">
+        <h2>Login</h2>
+        <form>
+            <div className="user-box">
+                <input type="text" name="" required />
+                <label>Username</label>
+            </div>
+            <div className="user-box">
+                <input type="password" name="" required />
+                <label>Password</label>
+                <a className="link-btn" href="/public/register.html">  Register</a>
+            </div>
+            <a href="#" className="asd">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Login
+            </a>
+        </form>
+    </div>
+    }
+}

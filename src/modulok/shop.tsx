0650b24581530cc9859be63f3a8eaa4f629c545d
DDomix <dominik.papp1010@gmail.com>
2023-03-22 13:50:42 +0100

    filter almost done

--- src/modulok/shop.tsx
+++ src/modulok/shop.tsx
@@ -1,7 +1,63 @@
-import { Component, ReactNode } from "react";
+import { getValue } from "@testing-library/user-event/dist/utils";
+import { Component, FormEvent, ReactNode } from "react";
+import './css/shopstyle.css';
 
-export default class Shop extends Component {
+interface State {
+    color:string;
+    size:string;
+    team:string;
+    name: string;
+}
+
+
+export default class Shop extends Component<{}, State> {
+    
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            color: '',
+            size: '',
+            team: '',
+            name: '',
+        }
+    }
+
+
+    kereses = async (e: FormEvent) => {
+        e.preventDefault();
+        console.log(e)
+        const loginData = {
+            'color': this.state.color,
+            'size': this.state.size,
+            'team': this.state.team,
+            'name': this.state.name,
+        };
+        
+        const response = await fetch('http://localhost:3000/api/shop', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            //body: JSON.stringify(loginData),
+        });
+
+        console.log(response);
+    }
+
+
     render(): ReactNode {
-        return <div></div>
+
+        const { color, size } = this.state;
+        
+        return <div className="shopstyle">
+            <input type="radio" name="szin" value={color} onChange={(e) => this.setState({ color: e.target.value="Fekete" })}/>Fekete
+            <input type="radio" name="szin" value={color} onChange={(e) => this.setState({ color: e.target.value="Fehér" })}/>Fehér
+            <br/>
+            <input type="radio" id="S" name="meret" value={size} onChange={(e) => this.setState({ size: e.target.value="S" })} />S
+            <input type="radio" id="M" name="meret" value={size} onChange={(e) => this.setState({ size: e.target.value="M" })} />M
+            <input type="radio" id="L" name="meret" value={size} onChange={(e) => this.setState({ size: e.target.value="L" })} />L
+
+            <button onClick={this.kereses}>Keresés</button>
+        </div>
     }
 }
